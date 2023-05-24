eed5572e0a785a10b99580d338469c6fcabee2de
Maarij-Camroodien <maarijcamroodien18@gmail.com>
2023-05-24 16:02:38 +0200

    q1

---
+++ script/code.js
@@ -0,0 +1,15 @@
+//  1. Variables: containers for storing data 
+//  2. Value: A value is the representation of some entity that can be manipulated by the program it is in
+//  3. Data Types:  describe the different types or kinds of data that we're gonna be working with and storing in variables
+//  4. Initialisation: Storing a value in a variable
+//  5. Difference between var, let and const:var and let create variables that can be reassigned another value. const creates "constant" variables that cannot be reassigned another value
+//  6. What is the scope of a variable: the accessibility of a particular variable within the program
+//  7. When can you decide when to use a for or while, forEach and do while loop: A for Loop is used to repeat a specific block of code a number of times that is known.
+//     -A while Loop is used to repeat a specific block of code an unknown number of times until the conditions are met.
+//     -A foreach loop is used when you need to operate on each item individually in an array.
+//     -A do while loop is used in all scenarios where the loop body needs to be executed at least once.
+//  8. what is hoisting: the process whereby the interpreter appears to move the declaration of functions, variables or classes to the top of their scope, prior to execution of the code
+//    -What is variable scoping: the accessibility of a particular variable within the program
+
+let fullName = ('Maarij Camroodien')
+console.log(fullName);
